95f417103e9727a547078cf3179d7f3277b170b0
AlessandroFacci <alessandro.facci1996@gmail.com>
2023-08-30 09:51:51 +0200

    index and js: correction

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ createApp({
   data() {
     return {
       title: "Hello world !",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1200px-Vue.js_Logo_2.svg.png",
     };
   },
 }).mount("#app");
